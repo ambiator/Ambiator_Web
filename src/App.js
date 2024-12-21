0f2b289fb00874f69976c27ae09d980b51217c82
Mohan-Ganta <gantamohan.566@gmail.com>
2024-12-21 15:26:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,72 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import {
-//   HashRouter as Router, Routes, Route, Outlet, Navigate, useNavigate,
-// } from 'react-router-dom';
-// import HomePage from './Componats/HomePageComp/HomePage';
-// import { useEffect, useState } from 'react';
-// import UserResult from './Componats/UserComp/UserResult';
-// import DeviceMaster from './Componats/DeviceComp/DeviceMaster';
-// import DeviceConfigResult from './Componats/DeviceComp/DeviceConfig/DeviceConfigResult';
-// import ReportOfDevice from './Componats/AllReportComp/ReportOfDevice';
-// import MainDashboardComp from './Componats/DashboadComp/MainDashboardCompo';
-// import ApplicationStore from './Componats/Utility/localStorageUtil';
-// import LoginPage from './Componats/Login/Login';
-// import DeviceandLocation from './Componats/UserComp/DeviceandLocation/DeviceandLocation';
-// import AccessTable from './Componats/DashboadComp/DrillDown/AccessTable';
-// import { DataProvider } from './Componats/HomePageComp/DataProvider';
-
-// function ProtectedRoutes() {
-//   const navigate = useNavigate();
-//   const { accessToken, userDetails } = ApplicationStore().getStorage('userDetails');
-
-//   useEffect(() => {
-//     if (userDetails?.userRole === 'affiliate') {
-//       document.title = 'Affiliate Dashboard - My AMBIATOR';
-//     } else if (userDetails?.userRole === 'ambiator') {
-//       document.title = 'God Mode Dashboard - My AMBIATOR';
-//     } else if (userDetails?.userRole === 'customer') {
-//       document.title = 'My Dashboard - My AMBIATOR';
-//     } else {
-//       document.title = 'My AMBIATOR - Dashboard Login';
-//     }
-
-//   }, [userDetails, accessToken])
-
-
-//   if (accessToken) {
-//     return <Outlet />;
-//   }
-//   return <Navigate replace to="/login" />;
-// };
-
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       <Router>
-//         <Routes>
-//           <Route path="/login" element={<LoginPage />} />
-//           <Route element={<ProtectedRoutes />}>
-//             <Route path="/" element={<HomePage />}>
-//               <Route index element={<Navigate to="/MainDashboardComp" />} />
-//               <Route path="/MainDashboardComp" element={<MainDashboardComp />} />
-//               <Route path='/DeviceandLocation' element={<DeviceandLocation />} />
-//               <Route path='/AccessTable' element={<AccessTable />} />
-
-//             </Route>
-//           </Route>
-//         </Routes>
-//       </Router>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import { BrowserRouter as Router, Routes, Route, Outlet, Navigate, useNavigate } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import {
+  HashRouter as Router, Routes, Route, Outlet, Navigate, useNavigate,
+} from 'react-router-dom';
 import HomePage from './Componats/HomePageComp/HomePage';
 import { useEffect, useState } from 'react';
 import UserResult from './Componats/UserComp/UserResult';
@@ -95,7 +31,8 @@ function ProtectedRoutes() {
       document.title = 'My AMBIATOR - Dashboard Login';
     }
 
-  }, [userDetails, accessToken]);
+  }, [userDetails, accessToken])
+
 
   if (accessToken) {
     return <Outlet />;
@@ -103,7 +40,9 @@ function ProtectedRoutes() {
   return <Navigate replace to="/login" />;
 };
 
+
 function App() {
+
   return (
     <div className="App">
       <Router>
@@ -113,8 +52,9 @@ function App() {
             <Route path="/" element={<HomePage />}>
               <Route index element={<Navigate to="/MainDashboardComp" />} />
               <Route path="/MainDashboardComp" element={<MainDashboardComp />} />
-              <Route path="/DeviceandLocation" element={<DeviceandLocation />} />
-              <Route path="/AccessTable" element={<AccessTable />} />
+              <Route path='/DeviceandLocation' element={<DeviceandLocation />} />
+              <Route path='/AccessTable' element={<AccessTable />} />
+
             </Route>
           </Route>
         </Routes>
--- src/Componats/Login/LogoinCustomerAbiator.js
+++ src/Componats/Login/LogoinCustomerAbiator.js
@@ -1,211 +1,5 @@
-// import React, { useEffect, useState, } from 'react'
-// import { Grid, Avatar, TextField, Button, Stack, Typography, Card, CardContent, Input, FormControl, } from '@mui/material'
-// import Hidden from '@mui/material/Hidden';
-// import { useNavigate } from 'react-router-dom';
-// import { LoginService } from '../ApiService/LoginPageService';
-// import NotificationBar from '../notification/ServiceNotificationBar';
-// import ApplicationStore from '../Utility/localStorageUtil';
-// import AmbiatorIcon from '../../Images/ambiatorLogo.jpg';
-// import AmbiatorLogo from '../../Images/AmbiatorLogo.png';
-
-// const LogoinCustomerAbiator = () => {
-//     const navigate = useNavigate();
-//     const [openNotification, setNotification] = useState({
-//         status: false,
-//         type: 'error',
-//         message: 'Login Successful',
-//     });
-
-//     useEffect(() => {
-//         const userDetails = JSON.parse(sessionStorage.getItem("userDetails"));
-//         return userDetails?.accessToken ? navigate("/") : navigate("/login");
-//     }, []);
-
-//     const successCaseCode = [200, 201];
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-
-
-//     function submit(e) {
-//         e.preventDefault();
-//         LoginService({ email: email, password: password, userRole: "ambiator" })
-//             .then((response) => {
-//                 if (successCaseCode.indexOf(response.status) > -1) {
-//                     console.log('Login Succesfull..!');
-//                     setNotification({
-//                         status: true,
-//                         type: 'success',
-//                         message: 'Login Succesfull..!',
-//                     });
-
-//                     return response.json();
-//                 }
-//                 throw {
-//                     errorStatus: response.status,
-//                     errorObject: response.json(),
-//                 };
-//             }).then((data) => {
-//                 ApplicationStore().setStorage('userDetails', data);
-//                 setTimeout(() => {
-//                     navigate("/MainDashboardComp");
-//                 }, 2000);
-//             }).catch((error) => {
-//                 error?.errorObject?.then((errorResponse) => {
-//                     console.log(errorResponse.error ? errorResponse.error : errorResponse.message);
-//                     setNotification({
-//                         status: true,
-//                         type: 'error',
-//                         message: errorResponse.error ? errorResponse.error : errorResponse.message,
-//                     });
-//                     setTimeout(() => {
-//                         handleClose();
-//                     }, 2000);
-//                 });
-//             });
-
-//     }
-
-
-//     const avatarStyle = { backgroundColor: '#3B3531' }
-//     const handleCloseNotify = () => {
-//         setNotification({
-//             status: false,
-//             type: '',
-//             message: '',
-//         });
-//     };
-
-//     const handleClose = () => {
-//         setNotification({
-//             status: false,
-//             type: '',
-//             message: '',
-//         });
-//     };
-
-//     return (
-//         <form onSubmit={submit}>
-//             <Grid container spacing={2}>
-//                 <Grid item style={{ height: '70vh', display: 'flex', alignItems: 'center' }} xs={12} sm={12} md={5} lg={5} xl={5} >
-//                     <Grid container spacing={2} style={{ display: 'flex', flexDirection: 'column' }} >
-//                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12} align='center' >
-//                             <img
-//                                 style={{
-//                                     alignSelf: 'center',
-//                                     textAlign: 'center',
-//                                     fontWeight: 'bold',
-//                                     // width: '200px',
-//                                     // height: '120px'
-//                                     width: '130px',
-//                                     height: 'auto'
-//                                 }} src={AmbiatorIcon} />
-//                         </Grid>
-//                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12} align='center' >
-//                             <Typography variant="h6" style={{ alignSelf: 'center', textAlign: 'center', fontWeight: 'bold' }}> Sign in to your account </Typography>
-//                         </Grid>
-//                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12}>
-//                             <FormControl fullWidth>
-
-//                                 <Input
-//                                     type='email'
-//                                     id="email"
-//                                     label="Email"
-//                                     variant="outlined"
-//                                     placeholder='Enter User Email'
-//                                     required
-//                                     fullWidth
-//                                     name='email'
-
-//                                     onChange={(e) => {
-
-//                                         setEmail(e.target.value);
-//                                     }
-//                                     }
-//                                     value={email}
-//                                 />
-//                             </FormControl>
-
-//                         </Grid>
-//                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12}>
-//                             <FormControl fullWidth>
-//                                 <Input
-//                                     id="password"
-//                                     margin="normal"
-//                                     label="Password"
-//                                     variant="outlined"
-//                                     placeholder='Enter password'
-//                                     required
-//                                     fullWidth
-//                                     type='password'
-//                                     name='password'
-//                                     onChange={(e) => {
-
-//                                         setPassword(e.target.value);
-//                                     }
-//                                     }
-//                                     value={password} />
-//                             </FormControl>
-
-
-//                         </Grid>
-//                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12} >
-//                             <Button style={{ alignSelf: 'center', textAlign: 'center', backgroundColor: '#602bf9', borderRadius: '25px' }}
-//                                 type="submit"
-//                                 fullWidth
-//                                 variant="contained"
-//                             >
-//                                 Sign In
-//                             </Button>
-//                         </Grid>
-//                     </Grid>
-
-//                 </Grid>
-//                 <Hidden only={["sm", 'xs']}>
-//                     <Grid item sx={12} sm={7} md={6} lg={7} xl={7} style={{ display: 'flex', alignItems: 'center' }}>
-//                         {/* <Card style={{
-//                             backgroundColor: '#dfe1f7',
-//                             display: 'flex',
-//                             alignItems: 'center',
-//                             width: '80vw',
-//                             height: '86vh',
-//                             boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)', // Adjust the values as needed
-//                             transition: 'transform 0.3s',
-//                             '&:hover': {
-//                                 transform: 'scale(1.05)', // Adjust the scaling factor as needed
-//                             }
-//                         }}> */}
-
-//                         <CardContent >
-//                             <Stack
-//                                 direction="row"
-//                                 justifyContent="center"
-//                                 alignItems="center"
-//                                 spacing={2}
-
-//                             >
-//                                 <img src={AmbiatorLogo} alt="React Logo" height="100%" width="100%" />
-//                             </Stack>
-
-//                         </CardContent>
-//                         {/* </Card> */}
-//                     </Grid>
-//                 </Hidden>
-//                 <NotificationBar
-//                     handleClose={handleClose}
-//                     notificationContent={openNotification.message}
-//                     openNotification={openNotification.status}
-//                     type={openNotification.type}
-//                 />
-//             </Grid>
-//         </form>
-//     )
-// }
-
-// export default LogoinCustomerAbiator
-
-
-import React, { useEffect, useState } from 'react';
-import { Grid, Avatar, TextField, Button, Stack, Typography, Card, CardContent, Input, FormControl } from '@mui/material';
+import React, { useEffect, useState, } from 'react'
+import { Grid, Avatar, TextField, Button, Stack, Typography, Card, CardContent, Input, FormControl, } from '@mui/material'
 import Hidden from '@mui/material/Hidden';
 import { useNavigate } from 'react-router-dom';
 import { LoginService } from '../ApiService/LoginPageService';
@@ -231,13 +25,13 @@ const LogoinCustomerAbiator = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+
     function submit(e) {
         e.preventDefault();
         LoginService({ email: email, password: password, userRole: "ambiator" })
             .then((response) => {
                 if (successCaseCode.indexOf(response.status) > -1) {
                     console.log('Login Succesfull..!');
-
                     setNotification({
                         status: true,
                         type: 'success',
@@ -246,26 +40,40 @@ const LogoinCustomerAbiator = () => {
 
                     return response.json();
                 }
-
-                throw new Error(`Error Status: ${response.status}, Error Object: ${response.json()}`);
+                throw {
+                    errorStatus: response.status,
+                    errorObject: response.json(),
+                };
             }).then((data) => {
                 ApplicationStore().setStorage('userDetails', data);
                 setTimeout(() => {
                     navigate("/MainDashboardComp");
                 }, 2000);
             }).catch((error) => {
-                error?.message ? console.log(error.message) : console.log('Unknown Error');
-                setNotification({
-                    status: true,
-                    type: 'error',
-                    message: error?.message || 'An unexpected error occurred.',
+                error?.errorObject?.then((errorResponse) => {
+                    console.log(errorResponse.error ? errorResponse.error : errorResponse.message);
+                    setNotification({
+                        status: true,
+                        type: 'error',
+                        message: errorResponse.error ? errorResponse.error : errorResponse.message,
+                    });
+                    setTimeout(() => {
+                        handleClose();
+                    }, 2000);
                 });
-
-                setTimeout(() => {
-                    handleClose();
-                }, 2000);
             });
+
     }
+
+
+    const avatarStyle = { backgroundColor: '#3B3531' }
+    const handleCloseNotify = () => {
+        setNotification({
+            status: false,
+            type: '',
+            message: '',
+        });
+    };
 
     const handleClose = () => {
         setNotification({
@@ -282,22 +90,22 @@ const LogoinCustomerAbiator = () => {
                     <Grid container spacing={2} style={{ display: 'flex', flexDirection: 'column' }} >
                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12} align='center' >
                             <img
-                                src={AmbiatorIcon}
-                                alt="Ambiator Icon"
                                 style={{
                                     alignSelf: 'center',
                                     textAlign: 'center',
                                     fontWeight: 'bold',
+                                    // width: '200px',
+                                    // height: '120px'
                                     width: '130px',
                                     height: 'auto'
-                                }}
-                            />
+                                }} src={AmbiatorIcon} />
                         </Grid>
                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12} align='center' >
                             <Typography variant="h6" style={{ alignSelf: 'center', textAlign: 'center', fontWeight: 'bold' }}> Sign in to your account </Typography>
                         </Grid>
                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12}>
                             <FormControl fullWidth>
+
                                 <Input
                                     type='email'
                                     id="email"
@@ -307,10 +115,16 @@ const LogoinCustomerAbiator = () => {
                                     required
                                     fullWidth
                                     name='email'
-                                    onChange={(e) => setEmail(e.target.value)}
+
+                                    onChange={(e) => {
+
+                                        setEmail(e.target.value);
+                                    }
+                                    }
                                     value={email}
                                 />
                             </FormControl>
+
                         </Grid>
                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12}>
                             <FormControl fullWidth>
@@ -324,10 +138,15 @@ const LogoinCustomerAbiator = () => {
                                     fullWidth
                                     type='password'
                                     name='password'
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    value={password}
-                                />
+                                    onChange={(e) => {
+
+                                        setPassword(e.target.value);
+                                    }
+                                    }
+                                    value={password} />
                             </FormControl>
+
+
                         </Grid>
                         <Grid item sx={12} sm={12} md={12} lg={12} xl={12} >
                             <Button style={{ alignSelf: 'center', textAlign: 'center', backgroundColor: '#602bf9', borderRadius: '25px' }}
@@ -339,19 +158,36 @@ const LogoinCustomerAbiator = () => {
                             </Button>
                         </Grid>
                     </Grid>
+
                 </Grid>
                 <Hidden only={["sm", 'xs']}>
                     <Grid item sx={12} sm={7} md={6} lg={7} xl={7} style={{ display: 'flex', alignItems: 'center' }}>
+                        {/* <Card style={{
+                            backgroundColor: '#dfe1f7',
+                            display: 'flex',
+                            alignItems: 'center',
+                            width: '80vw',
+                            height: '86vh',
+                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)', // Adjust the values as needed
+                            transition: 'transform 0.3s',
+                            '&:hover': {
+                                transform: 'scale(1.05)', // Adjust the scaling factor as needed
+                            }
+                        }}> */}
+
                         <CardContent >
                             <Stack
                                 direction="row"
                                 justifyContent="center"
                                 alignItems="center"
                                 spacing={2}
+
                             >
-                                <img src={AmbiatorLogo} alt="Ambiator Logo" height="100%" width="100%" />
+                                <img src={AmbiatorLogo} alt="React Logo" height="100%" width="100%" />
                             </Stack>
+
                         </CardContent>
+                        {/* </Card> */}
                     </Grid>
                 </Hidden>
                 <NotificationBar
@@ -362,7 +198,7 @@ const LogoinCustomerAbiator = () => {
                 />
             </Grid>
         </form>
-    );
-};
+    )
+}
 
-export default LogoinCustomerAbiator;
+export default LogoinCustomerAbiator
